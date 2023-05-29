20020fc58f9f0a7dae475077b7f7cda9063fcce7
morshedalam39 <morshedalam4239@gmail.com>
2023-05-29 11:35:15 +0600

    get all menu from database

--- src/main.jsx
+++ src/main.jsx
@@ -8,14 +8,25 @@ import {
 import "./index.css";
 import router from './Routes/Routes';
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './providers/AuthProviders';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className='max-w-screen-xl mx-auto'>
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <QueryClientProvider client={queryClient}>
+          <div className='max-w-screen-xl mx-auto'>
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
