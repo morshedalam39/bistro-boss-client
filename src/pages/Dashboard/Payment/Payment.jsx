f3bb976f35aa2056444f3c5d103adf400aea99c8
morshedalam39 <morshedalam4239@gmail.com>
2023-06-05 11:36:00 +0600

    paument added

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import { loadStripe } from "@stripe/stripe-js";
+import SectionTitle from "../../../components/SectionTitle";
 
+import { Elements } from "@stripe/react-stripe-js";
+import useCart from "../../../hooks/useCart";
+import CheckoutForm from "./CheckoutForm";
+
+
+// TODO: provide publishable Key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 const Payment = () => {
+    const [cart] = useCart();
+    const total = cart.reduce((sum, item) => sum + item.price, 0);
+    const price = parseFloat(total.toFixed(2))
     return (
         <div>
-            <h1>Taka</h1>
+            <SectionTitle subHeading="please process" heading="Payment"></SectionTitle>
+            <h2 className="text-3xl"> Teka o teka tumi uira uira aso...</h2>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm cart={cart} price={price}></CheckoutForm>
+            </Elements>
         </div>
     );
 };
